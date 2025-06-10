1e378055e251a9a34d30c5ff95bd4c0a2eb20881
Ernest Mallett <Emallett@ariessys.com>
2025-06-10 11:02:31 -0500

    Updated dns module and nginx module

---
+++ modules/dns/providers.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    nginxproxymanager = {
+      source = "Sander0542/nginxproxymanager"
+      version = "1.1.1"
+    }
+
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 5"
+    }
+
+    technitium = {
+      source = "kevynb/technitium"
+      version = "0.2.0"
+    }
+  }
+}
