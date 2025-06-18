11c1e040fee0c4349bb4e424bf707c9d766aa346
Ernest Mallett <Emallett@ariessys.com>
2025-06-18 02:51:10 -0500

    Update arr services

--- services/arrr/locals.tf
+++ services/arrr/locals.tf
@@ -1,8 +1,22 @@
 locals {
-  domain_name="test.mallett.family"
-  ip_address = "192.168.5.13"
+  zone_name = "mallett.family"
+  
   username = "default_user"
   password = "SecretPassword1!"
+
+  proxy_ip = "192.168.4.2"
   authentik_ip = "192.168.4.55"
-  service_port = 9696
+  authentik_port = 9000
+
+  services = {
+    1 = {
+      service_name = "prowlarr"
+      image_name = "linuxserver/prowlarr:1.37.0"
+      username = "prowlarr_user"
+      password = "ProwlarrPassword1!"
+      domain_name = "prowlarr.mallett.family"
+      service_port = 9696
+      ip_address = "192.168.5.13"
+    }
+  }
 }
--- services/arrr/main.tf
+++ services/arrr/main.tf
@@ -1,22 +1,24 @@
 module "service_docker" {
+    for_each = local.services
   source = "../../modules/docker"
   
-  container_name = "AAutomated_Test"
-  container_image = "linuxserver/prowlarr:1.37.0"
+  container_name = each.value.service_name
+  container_image = each.value.image_name
   attach_to_br0 = false
   attach_to_br1 = true
-  br1_ipv4_addr = local.ip_address
+  br1_ipv4_addr = each.value.ip_address
 }
 
 module "service_dns" {
   source = "../../modules/dns"
+  for_each = local.services
   
   internal_only = true
-  service_port = 9000//local.service_port  - If using proxy_auth it needs to be the port to authentik
-  zone_name = "mallett.family"
-  domain_name = local.domain_name
+  service_port = local.authentik_port
+  zone_name = local.zone_name
+  domain_name = each.value.domain_name
   access_list_id = var.access_list_id
-  internal_host_ipv4 = "192.168.4.2" //Port to Nginx
+  internal_host_ipv4 = local.proxy_ip
   service_ipv4 = local.authentik_ip
   admin_email = var.admin_email
   dns_cloudflare_api_token = var.cloudflare_token
@@ -27,19 +29,20 @@ module "service_dns" {
 
 module "authentication" {
   source = "../../modules/proxy_auth"
+  for_each = local.services
   
-  internal_host = "http://${local.ip_address}:${local.service_port}"
-  external_host = local.domain_name
-  name = "prowlarr"
-  username_attribute = "prowlarr_username"
-  password_attribute = "prowlarr_password"
+  internal_host = "http://${each.value.ip_address}:${each.value.service_port}"
+  external_host = each.value.domain_name
+  name = each.value.service_name
+  username_attribute = "${each.value.service_name}_username"
+  password_attribute = "${each.value.service_name}_password"
   create_access_group = true
-  access_group_name = "terraform_prowlarr"
+  access_group_name = "tf_${each.value.service_name}"
   user_to_add_to_access_group = var.admin_username
   access_group_attributes = jsonencode(
     {
-      prowlarr_username: local.username,
-      prowlarr_password: local.password
+      "${each.value.service_name}_username": local.username,
+      "${each.value.service_name}_password": local.password
     }
   )
 }
