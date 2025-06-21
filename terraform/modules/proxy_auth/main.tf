21ac0a494230419b6fabbde33093c64d670ad1aa
Ernest Mallett <ernest.mallett@dccoder.com>
2025-06-21 10:32:08 -0500

    Remove creation of proxy outpost

--- terraform/modules/proxy_auth/main.tf
+++ terraform/modules/proxy_auth/main.tf
@@ -7,9 +7,14 @@ terraform {
   }
 }
 
+/*
+
+No easy way to do this, have to manually assign applications to the outpost.  
+See: https://github.com/goauthentik/terraform-provider-authentik/issues/310 
 resource "authentik_outpost" "outpost" {
   name = "tf_${var.name}_outpost"
   protocol_providers = [
     authentik_provider_proxy.name.id
   ]
 }
+*/
