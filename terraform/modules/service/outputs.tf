448df7cf1ae642ec63db278319c5129191020429
Ernest Mallett <Emallett@ariessys.com>
2025-06-24 04:18:23 -0500

    Working on service and stack

--- terraform/modules/service/outputs.tf
+++ terraform/modules/service/outputs.tf
@@ -3,3 +3,21 @@ output "generated_secrets" {
   value       = local.generated_secrets
   sensitive   = true
 }
+
+output "client_id" {
+  description = "The OAuth client ID for the service, if OAuth is enabled."
+  value       = module.oauth_authentication.client_id
+  sensitive   = true
+}
+
+output "client_secret" {
+  description = "The OAuth client secret for the service, if OAuth is enabled."
+  value       = module.oauth_authentication.client_secret
+  sensitive   = true
+}
+
+output "well_known_url" {
+  description = "The well-known URL for the OAuth service, if OAuth is enabled."
+  value       = module.oauth_authentication.provider_info_url
+  sensitive   = true
+}
