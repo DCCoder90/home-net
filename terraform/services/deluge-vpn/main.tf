c667eec0bc8839523c4ab2c076bb3126f4ab1f8a
Ernest Mallett <Emallett@ariessys.com>
2025-06-24 03:52:15 -0500

    Update deluge-vpn module

--- terraform/services/deluge-vpn/main.tf
+++ terraform/services/deluge-vpn/main.tf
@@ -5,9 +5,7 @@ module "delugevpn_service" {
   web_ui          = "http://${var.service.ip_address}:${var.service.service_port}"
   container_name  = var.service.service_name
   container_image = var.service.image_name
-  attach_to_br0   = false
-  attach_to_br1   = true
-  br1_ipv4_addr   = var.service.ip_address
+  networks = var.service.networks
   environment_vars = concat(var.service.env, [
     "VPN_USER=${var.vpn_user}",
     "VPN_PASS=${var.vpn_pass}"
