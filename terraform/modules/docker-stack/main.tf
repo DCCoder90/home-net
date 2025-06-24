d119bdfb260de46a5b3bc742ebe763562f8fc990
Ernest Mallett <Emallett@ariessys.com>
2025-06-24 04:11:14 -0500

    Start cleanup of docker-stack

--- terraform/modules/docker-stack/main.tf
+++ terraform/modules/docker-stack/main.tf
@@ -12,26 +12,10 @@ module "custom_network" {
   networks = local.creatable_networks
 }
 
-module "service_container" {
+module "service" {
   for_each = var.stack.services
-  source   = "../../modules/docker"
+  source   = "../../modules/service"
 
-  icon = each.value.icon
-  web_ui = (
-    each.value.network != null && each.value.network.ip_address != null && each.value.network.service_port != null ?
-    "http://${each.value.network.ip_address}:${each.value.network.service_port}" : null
-  )
-  container_name         = each.value.service_name
-  container_image        = each.value.image_name
-  container_network_mode = each.value.network_mode
-  environment_vars       = toset(concat(coalesce(var.stack.env, []), local.processed_envs[each.key], coalesce(var.stack.env, []), coalesce(local.oauth_envs[each.key], [])))
-  mounts                 = concat(coalesce(var.stack.mounts, []), coalesce(each.value.mounts, []))
-  container_capabilities = each.value.capabilities
-  commands               = each.value.commands
-
-  # Attach the container to custom networks defined in the stack, but only if the service
-  # explicitly lists that network in its own configuration.
-  networks = length(local.creatable_networks) > 0 ? [
-    for net in coalesce(each.value.network.networks, []) : module.custom_network[0].networks[net.name].id
-  ] : []
+  service = each.value
+  system = var.system
 }
