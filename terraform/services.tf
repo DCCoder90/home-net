192a32efb25692c3f58ad2244277945d6fd5482e
Ernest Mallett <1358226+DCCoder90@users.noreply.github.com>
2025-07-08 02:48:29 -0500

    Create docker-service module (#39)
    * Started on docker-service module
    
    * Comment out a bunch of stuff that I'll be eventually removing
    
    * Update to docker-stack
    
    * Added docker-service module and removed generated_secrets
    
    * Fix docker service issues
    
    * Change module name to client_id
    
    * Updated docs
    
    * Testing update
    
    * Update to services

--- terraform/services.tf
+++ terraform/services.tf
@@ -1,31 +1,15 @@
-module "secrets" {
-  source = "./modules/generated_secrets"
-
-  name = local.secrets
-}
-
 module "stacks" {
   for_each = local.stacks
   source   = "./modules/docker-stack"
 
   stack  = each.value
   system = local.system
-
-  //depends_on = [ module.secrets ]
 }
 
-module "flaresolverr_service" {
-  source = "./modules/docker"
+module "services"{
+  for_each = local.services
+  source   = "./modules/docker-service"
 
-  container_name   = local.services.flaresolverr.service_name
-  container_image  = local.services.flaresolverr.image_name
-  networks         = local.services.flaresolverr.network.networks
-  environment_vars = local.services.flaresolverr.env
-}
-
-module "delugevpn_service" {
-  source   = "./services/deluge-vpn"
-  service  = local.services.deluge-vpn
-  vpn_pass = var.vpn_pass
-  vpn_user = var.vpn_user
+  service  = each.value
+  system   = local.system
 }
