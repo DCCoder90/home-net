ba707a9c80a387f8e93a720baa7e303586a7c81f
Ernest Mallett <ernest.mallett@dccoder.com>
2026-01-27 15:27:31 -0600

    Docker image doesn't support imports

--- terraform/apps/imports.tf
+++ terraform/apps/imports.tf
@@ -3,12 +3,6 @@
 # Remove this file after a successful apply imports them into state.
 # -------------------------------------------------------------------
 
-# ---- Vaultwarden ----
-import {
-  to = module.services["vaultwarden"].module.service_container.docker_image.main
-  id = "sha256:032fced6c02ed83d6a5da88a2c629de425788ce1c6cc45775766d7cfbeb9f054"
-}
-
 import {
   to = module.services["vaultwarden"].module.service_container.docker_container.container
   id = "1544936f387df2886202cd24d9923939674a1dad8d7ba2b3d46e62d07723218e"
